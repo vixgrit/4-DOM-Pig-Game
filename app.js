85867c52556af172bd5af3b10fa4a5fe249f84ba
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-18 01:38:29 -0400

    implement win condition logic and UI

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
-
-
 document.querySelector('.btn-roll').addEventListener('click', function() {
   //random number
   var dice = Math.floor(Math.random() * 6) + 1;
@@ -37,10 +35,37 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   }
   else {
     //next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-    document.getElementById('current-0').textContent = '0';;
-    document.getElementById('current-1').textContent = '0';;
-
+    nextPlayer();
   }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  //add current score to global score-0
+  scores[activePlayer] += roundScore;
+  //update UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+  //check if player won the game
+  if (scores[activePlayer] >= 100) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+  }
+  else {
+    //next player
+    nextPlayer();
+  }
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
