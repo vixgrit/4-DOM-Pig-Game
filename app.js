fc6955c245fb1472cd075e0f1c80a52b48704df0
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-18 01:20:40 -0400

    tally roundScore and reset upon dice roll of one

--- app.js
+++ app.js
@@ -29,6 +29,18 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   var diceDOM = document.querySelector('.dice');
   diceDOM.style.display = 'block';
   diceDOM.src = 'dice-' + dice + '.png';
-
   //update round score if rolled number was not a 1
+  if (dice !== 1) {
+    //add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  }
+  else {
+    //next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';;
+    document.getElementById('current-1').textContent = '0';;
+
+  }
 });
